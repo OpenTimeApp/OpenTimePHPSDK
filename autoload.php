054aaa1f5a2429b953ec02be395af89fe8995c06
Josh Woodcock <josh.woodcock@opentimeapp.com>
2016-05-11 11:10:53 -0500

    Added organization api to autoload

--- autoload.php
+++ autoload.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__.'/src/api/OpenTimeSDK.php';
 require_once __DIR__.'/src/apis/meeting/OTMeetingAPI.php';
+require_once __DIR__.'/src/apis/organization/OTOrganizationAPI.php';
 require_once __DIR__.'/src/apis/user/OTUserAPI.php';
 
 ?>

20489cda2ad47801a915f26bb3649821554a4521
Josh Woodcock <josh.woodcock@opentimeapp.com>
2016-05-11 19:38:15 -0500

    Removed org id from deserialized user invite

--- src/deserializers/OTDeserializedUserInvite.php
+++ src/deserializers/OTDeserializedUserInvite.php
@@ -2,7 +2,6 @@
 
 class OTDeserializedUserInvite {
 
-	private $_orgId;
 	private $_email;
 	private $_lastInvitedAt;
 	private $_declinedAt;
@@ -14,7 +13,6 @@ class OTDeserializedUserInvite {
 	 * @param $data stdClass
 	 */
 	public function __construct($data) {
-		$this->_orgId         = isset($data->org_id) ? $data->org_id : null;
 		$this->_email         = isset($data->email) ? $data->email : null;
 		$this->_lastInvitedAt = isset($data->last_invited_at) ? $data->last_invited_at : 0;
 		$this->_declinedAt    = isset($data->declined_at) ? $data->declined_at : 0;
@@ -22,13 +20,6 @@ class OTDeserializedUserInvite {
 	}
 
 	/**
-	 * @return int
-	 *
-	 * public function getOrgId() {
-	 * return $this->_orgId;
-	 * }
-	 *
-	 * /**
 	 * @return string
 	 */
 	public function getEmail() {

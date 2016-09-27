a27b4875e55a43828851c0e4dc0cf86c33fa01c3
Josh Woodcock <josh.woodcock@opentimeapp.com>
2016-09-26 19:20:10 -0500

    Added ability set whether or not the emails would also be invited to organization

--- src/apis/group/invite/OTGroupInviteStatusRequest.php
+++ src/apis/group/invite/OTGroupInviteStatusRequest.php
@@ -1,15 +1,16 @@
 <?php
-require_once dirname(dirname(dirname(__DIR__ ))). '/helpers/OTGroupInviteValidationHelper.php';
+require_once dirname(dirname(dirname(__DIR__))) . '/helpers/OTGroupInviteValidationHelper.php';
 
 class OTGroupInviteStatusRequest {
 
 	private $_emails;
 	private $_groupId;
-
+	private $_alsoInviteToOrg;
 
 	public function __construct($emails, $groupId) {
-		$this->_emails = $emails;
-		$this->_groupId = $groupId;
+		$this->_emails          = $emails;
+		$this->_groupId         = $groupId;
+		$this->_alsoInviteToOrg = false;
 	}
 
 	public function checkInputs() {
@@ -19,10 +20,21 @@ class OTGroupInviteStatusRequest {
 	public function getParameters() {
 
 		$arr = array(
-			'emails' => $this->_emails,
-			'group_id' => $this->_groupId,
+			'emails'             => $this->_emails,
+			'group_id'           => $this->_groupId,
+			'also_invite_to_org' => $this->_alsoInviteToOrg
 		);
 
 		return $arr;
+	}
+
+	/**
+	 * @param boolean $also_invite
+	 *
+	 * @return OTGroupInviteStatusRequest
+	 */
+	public function setAlsoInviteToOrg($also_invite){
+		$this->_alsoInviteToOrg = $also_invite;
+		return $this;
 	}
 }
--- tests/apis/OTGroupAPITest.php
+++ tests/apis/OTGroupAPITest.php
@@ -29,6 +29,7 @@ class OTGroupAPITest extends OTAPITestParent {
 
 	public function testGetInviteStatusWithEmptyRequest(){
 		$request = new OTGroupInviteStatusRequest(',', 1);
+		$request->setAlsoInviteToOrg(true);
 		$response = OTGroupAPI::getInviteStatusOfEmails($request);
 		$this->assertTrue($response->success, $response->message);
 		$this->assertTrue(is_array($response->data));
